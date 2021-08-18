5c2b501a55e37db01e9645c935c27ca3508854c2
oleg <o01eg@yandex.ru>
2021-08-18 14:19:19 +0300

    Check format

--- webrtc/conn.js
+++ webrtc/conn.js
@@ -1,4 +1,5 @@
 var logElem = null;
+const ipregex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gm;
 
 function log(message) {
 	if (logElem) {
@@ -7,7 +8,7 @@ function log(message) {
 }
 
 function fakeAnswer(connHost, connPort) {
-	let sdp = "";
+	let sdp = "v=0\r\n";
 	return {
 		type: "answer",
 		sdp
@@ -19,20 +20,20 @@ function connect(connHost, connPort) {
 	log("Created connection");
 	let dataChannel = conn.createDataChannel("chat");
 	log("Created data channel");
-	conn.createOffer().catch(function(reason) {
-		log(`Cannot create offer: ${reason}`);
-	}).then(function(offer) {
+	conn.createOffer().then(function(offer) {
 		log("Created offer");
-		conn.setLocalDescription(offer).catch(function(reason) {
-			log(`Cannot set offer: ${reason}`);
-		}).then(function(_) {
+		conn.setLocalDescription(offer).then(function(_) {
 			log("Set offer");
-			conn.setRemoteDescription(fakeAnswer(connHost, connPort)).catch(function(reason) {
-				log(`Cannot set answer: ${reason}`);
-			}).then(function(_) {
+			conn.setRemoteDescription(fakeAnswer(connHost, connPort)).then(function(_) {
 				log("Set answer");
+			}).catch(function(reason) {
+				log(`Cannot set answer: ${reason}`);
 			});
+		}).catch(function(reason) {
+			log(`Cannot set offer: ${reason}`);
 		});
+	}).catch(function(reason) {
+		log(`Cannot create offer: ${reason}`);
 	});
 }
 
@@ -44,11 +45,15 @@ function connToHost() {
 
 	logElem = document.getElementById("log");
 	logElem.innerHTML = "";
-	log(`Connecting to ${connHost}:${connPort} ...`);
-	try {
-		connect(connHost, connPort, log);
-	} catch (reason) {
-		log(`Connection error: ${reason}`);
+	if (connPort > 0 && ipregex.matches(connHost)) {
+		log(`Connecting to ${connHost}:${connPort} ...`);
+		try {
+			connect(connHost, connPort, log);
+		} catch (reason) {
+			log(`Connection error: ${reason}`);
+		}
+	} else {
+		log("Incorrect host and port");
 	}
 	return false;
 }
